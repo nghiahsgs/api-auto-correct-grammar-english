ccba8a4a9ffb0102684dae14e349fe729c9c35d3
unknown <nghiahsgs@gmail.com>
2020-04-23 10:52:33 +0700

    make it better

--- index.py
+++ index.py
@@ -21,5 +21,20 @@ def index():
     return render_template('index.html',data_render={'input_text':input_text,'output_text':output_text})
 
 
+@app.route('/check', methods=['GET','POST'])
+def check():
+    if request.method=="GET":
+        input_text=''
+        input_text_err=''
+        message=''
+    else:
+        input_text=request.form['input_text']
+        print('input_text',input_text)
+        input_text_err,message=utils.auto_check(input_text)
+        print('input_text_err',input_text_err)
+        print('message',message)
+    return render_template('check.html',data_render={'input_text':input_text,'input_text_err':input_text_err,'message':message})
+
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=3000)
--- utils.py
+++ utils.py
@@ -1,13 +1,31 @@
 from gingerit.gingerit import GingerIt
-
+import requests
 def auto_correct(text):
     # text = 'The smelt of fliwers bring back memories.'
     parser = GingerIt()
     result=parser.parse(text)
     return result['result'] 
 
-# text="I am feel verry happy"
-# result=auto_correct(text)
-# print(result)
+def auto_check(input_text):
+    # input_text='She’s married with an dentist.'  
+    url='https://languagetool.org/api/v2/check'
+    dataPost={'text':input_text,'language':'en-US'}
+    response=requests.post(url, data=dataPost)
+    result=response.json()
 
-# print(auto_correct())
+    if not len(result['matches']):
+        input_text_err=''
+        message='Perfect'
+    else:
+        offset=result['matches'][0]['offset']
+        length=result['matches'][0]['length']
+        input_text_err=input_text[offset:(offset+length)]
+        message=result['matches'][0]['message']
+    return input_text_err,message
+# print()
+
+input_text='She’s married to a dentist.'  
+url='https://languagetool.org/api/v2/check'
+dataPost={'text':input_text,'language':'en-US'}
+response=requests.post(url, data=dataPost)
+result=response.json()
